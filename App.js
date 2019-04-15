1fbc587b737d88146122976542a47deb4ee4c6b6
JM Fantin <joao.lerina@pucrs.br>
2019-04-15 13:45:03 -0300

    Working solution

--- App.js
+++ App.js
@@ -1,49 +1,49 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
-import Wizard from './src/components/Wizard'
+import Wizard from './src/components/Wizard';
+import Input from './src/components/Input';
+
+const forms = [
+  {
+    placeholder: 'Username here...',
+    name: 'username',
+  },
+  {
+    placeholder: 'Email here...',
+    name: 'email',
+  },
+  {
+    placeholder: 'Avatar here...',
+    name: 'avatar',
+  },
+];
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Wizard>
-          <Wizard.Step>
-            <View
-              style={{
-                flex: 1,
-                backgroundColor: 'red',
-                alignItems: 'center',
-                justifyContent: 'center'
-              }}
-            >
-              <Text>Put your username</Text>
-            </View>
-          </Wizard.Step>
-          <Wizard.Step>
-            <View
-              style={{
-                flex: 1,
-                backgroundColor: 'blue',
-                alignItems: 'center',
-                justifyContent: 'center'
-              }}
-            >
-              <Text>Put your email</Text>
-            </View>
-          </Wizard.Step>
-          <Wizard.Step>
-            <View
-              style={{
-                flex: 1,
-                backgroundColor: 'purple',
-                alignItems: 'center',
-                justifyContent: 'center'
-              }}
-            >
-              <Text>Select profile pic</Text>
-            </View>
-          </Wizard.Step>
+      <View style={styles.root}>
+        <Wizard
+          initialValues={{
+            username: '',
+            email: '',
+            avatar: '',
+          }}
+        >
+          {forms.map(el => (
+            <Wizard.Step key={el.name}>
+              {({ onChangeValue, values }) => (
+                <View style={styles.container}>
+                  <Input
+                    onChangeValue={onChangeValue}
+                    placeholder={el.placeholder}
+                    value={values[el.name]}
+                    name={el.name}
+                  />
+                </View>
+              )}
+            </Wizard.Step>
+          ))}
         </Wizard>
       </View>
     );
@@ -51,7 +51,12 @@ export default class App extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
   container: {
-    flex: 1
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
--- src/components/Wizard.js
+++ src/components/Wizard.js
@@ -1,67 +1,70 @@
-import React, { PureComponent } from 'react'
-import { View, Text, Button } from 'react-native'
+import React, { PureComponent } from 'react';
+import { View, Text, Button, Alert } from 'react-native';
 
-class Step extends PureComponent {
-    state = {}
-    render(){
-        return (
-            <View style={{ flex: 1 }}>
-                {this.props.children}
-                <Button
-                    title="Prev"
-                    disabled={this.props.currentIndex === 0}
-                    onPress={this.props.prevStep}
-                />
-                <Button
-                    title="Next"
-                    disabled={this.props.isLast}
-                    onPress={this.props.nextStep}
-                />
-            </View>
-        )
-    }
-}
+import Step from './Step';
 
 class Wizard extends PureComponent {
-    static step = props => <Step {...props}/>
-    state = {
-        index: 0
-    }
+  static Step = Step;
 
-    _nextStep = () => {
-        if(this.state.index != this.props.children.length - 1){
-            this.setState(prevState => ({
-                index: prevState.index + 1
-            }))
-        }
-    }
+  state = {
+    index: 0,
 
-    _prevStep = () => {
-        if(this.state.index != 0){
-            this.setState(prevState => ({
-                index: prevState.index - 1
-            }))
-        }
-    }
+    values: {
+      ...this.props.initialValues,
+    },
+  };
 
-    render() {
-        return(
-            <View style={{ flex: 1}}>
-                {React.Children.map(this.props.children, (el, index) => {
-                    if(index === this.state.index){
-                        return React.cloneElement(el, {
-                            currentIndex: this.state.index,
-                            nextStep: this._nextStep,
-                            prevStep: this._prevStep,
-                            isLast: this.state.index === this.props.children.length - 1,
-                        })
-                    } else {
-                        return null
-                    }
-                })}
-            </View>
-        )
+  _nextStep = () => {
+    if (this.state.index !== this.props.children.length - 1) {
+      this.setState(prevState => ({
+        index: prevState.index + 1,
+      }));
     }
+  };
+
+  _prevStep = () => {
+    if (this.state.index !== 0) {
+      this.setState(prevState => ({
+        index: prevState.index - 1,
+      }));
+    }
+  };
+
+  _onChangeValue = (name, value) => {
+    this.setState(prevState => ({
+      values: {
+        ...prevState.values,
+        [name]: value,
+      },
+    }));
+  };
+
+  _onSubmit = () => {
+    Alert.alert(JSON.stringify(this.state.values));
+  };
+
+  render() {
+    console.log('values', this.state);
+    return (
+      <View style={{ flex: 1 }}>
+        {React.Children.map(this.props.children, (el, index) => {
+          if (index === this.state.index) {
+            return React.cloneElement(el, {
+              currentIndex: this.state.index,
+              nextStep: this._nextStep,
+              prevStep: this._prevStep,
+              isLast: this.state.index === this.props.children.length - 1,
+              onChangeValue: this._onChangeValue,
+              values: this.state.values,
+              onSubmit: this._onSubmit,
+            });
+          }
+
+          return null;
+        })}
+      </View>
+    );
+  }
 }
 
-export default Wizard
+export default Wizard;
